2afd545ebe78f41cbdeb106087aca05ed2b35bb3
sykefu <sykefu@gmail.com>
2024-08-03 13:28:44 +0200

    figuring out svelte.

---
+++ frontend/src/lib/Comptest.svelte
@@ -0,0 +1,4 @@
+<script>
+    export let val = 12;
+</script>
+<p>This is another paragraph, valued at {val}.</p>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<!--svelte:head, added to header on page-->
+</svelte:head>
+
+<script>
+    import Comptest from "$lib/Comptest.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Comptest/>
+<Comptest val={1} />
